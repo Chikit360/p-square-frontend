3ab2c39a2fbb36ad102f17e7fcd62ffac8f5cae2
premprakashgupta <prem.com0011@gmail.com>
2025-05-26 13:45:57 +0530

    make strength as text field

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router";
 
 // Assume these icons are imported from an icon library
 import {
- 
+
   BoxIcon,
 
   ChevronDownIcon,
@@ -22,7 +22,7 @@ type NavItem = {
   name: string;
   icon: React.ReactNode;
   path?: string;
-  subItems?: { name: string; path: string; pro?: boolean; new?: boolean,role?:string[] }[];
+  subItems?: { name: string; path: string; pro?: boolean; new?: boolean, role?: string[] }[];
 };
 
 const navItems: NavItem[] = [
@@ -34,12 +34,12 @@ const navItems: NavItem[] = [
   {
     icon: <MedicineIcon />,
     name: "Medicine",
-    subItems: [{ name: "Items", path: "/medicine/items", pro: false },{ name: "Inventory", path: "/medicine/inventory", pro: false }],
+    subItems: [{ name: "Items", path: "/medicine/items", pro: false }, { name: "Inventory", path: "/medicine/inventory", pro: false }],
   },
   {
     icon: <BoxIcon />,
     name: "Sale",
-    subItems: [{ name: "Sale List", path: "/sale", pro: false },{ name: "Customer", path: "/customer-list", pro: false },],
+    subItems: [{ name: "Sale List", path: "/sale", pro: false }, { name: "Customer", path: "/customer-list", pro: false },],
   },
   // {
   //   icon: <CalenderIcon />,
@@ -76,8 +76,8 @@ const othersItems: NavItem[] = [
     icon: <PieChartIcon />,
     name: "Utility",
     subItems: [
-      { name: "Strength", path: "/admin/strength",role:["admin"], pro: false },
-      { name: "Form", path: "/admin/form",role:["admin"], pro: false },
+      // { name: "Strength", path: "/admin/strength",role:["admin"], pro: false },
+      { name: "Form", path: "/admin/form", role: ["admin"], pro: false },
     ],
   },
   // {
@@ -105,7 +105,7 @@ const othersItems: NavItem[] = [
 const AppSidebar: React.FC = () => {
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
   const location = useLocation();
-  const userRole=useSelector(getUserRole)
+  const userRole = useSelector(getUserRole)
 
   const [openSubmenu, setOpenSubmenu] = useState<{
     type: "main" | "others";
@@ -178,22 +178,19 @@ const AppSidebar: React.FC = () => {
           {nav.subItems ? (
             <button
               onClick={() => handleSubmenuToggle(index, menuType)}
-              className={`menu-item group ${
-                openSubmenu?.type === menuType && openSubmenu?.index === index
+              className={`menu-item group ${openSubmenu?.type === menuType && openSubmenu?.index === index
                   ? "menu-item-active"
                   : "menu-item-inactive"
-              } cursor-pointer ${
-                !isExpanded && !isHovered
+                } cursor-pointer ${!isExpanded && !isHovered
                   ? "lg:justify-center"
                   : "lg:justify-start"
-              }`}
+                }`}
             >
               <span
-                className={`menu-item-icon-size  ${
-                  openSubmenu?.type === menuType && openSubmenu?.index === index
+                className={`menu-item-icon-size  ${openSubmenu?.type === menuType && openSubmenu?.index === index
                     ? "menu-item-icon-active"
                     : "menu-item-icon-inactive"
-                }`}
+                  }`}
               >
                 {nav.icon}
               </span>
@@ -202,12 +199,11 @@ const AppSidebar: React.FC = () => {
               )}
               {(isExpanded || isHovered || isMobileOpen) && (
                 <ChevronDownIcon
-                  className={`ml-auto w-5 h-5 transition-transform duration-200 ${
-                    openSubmenu?.type === menuType &&
-                    openSubmenu?.index === index
+                  className={`ml-auto w-5 h-5 transition-transform duration-200 ${openSubmenu?.type === menuType &&
+                      openSubmenu?.index === index
                       ? "rotate-180 text-brand-500"
                       : ""
-                  }`}
+                    }`}
                 />
               )}
             </button>
@@ -215,16 +211,14 @@ const AppSidebar: React.FC = () => {
             nav.path && (
               <Link
                 to={nav.path}
-                className={`menu-item group ${
-                  isActive(nav.path) ? "menu-item-active" : "menu-item-inactive"
-                }`}
+                className={`menu-item group ${isActive(nav.path) ? "menu-item-active" : "menu-item-inactive"
+                  }`}
               >
                 <span
-                  className={`menu-item-icon-size ${
-                    isActive(nav.path)
+                  className={`menu-item-icon-size ${isActive(nav.path)
                       ? "menu-item-icon-active"
                       : "menu-item-icon-inactive"
-                  }`}
+                    }`}
                 >
                   {nav.icon}
                 </span>
@@ -252,32 +246,29 @@ const AppSidebar: React.FC = () => {
                   <li key={subItem.name}>
                     <Link
                       to={subItem.path}
-                      className={`menu-dropdown-item ${
-                        isActive(subItem.path)
+                      className={`menu-dropdown-item ${isActive(subItem.path)
                           ? "menu-dropdown-item-active"
                           : "menu-dropdown-item-inactive"
-                      }`}
+                        }`}
                     >
                       {subItem.name}
                       <span className="flex items-center gap-1 ml-auto">
                         {subItem.new && (
                           <span
-                            className={`ml-auto ${
-                              isActive(subItem.path)
+                            className={`ml-auto ${isActive(subItem.path)
                                 ? "menu-dropdown-badge-active"
                                 : "menu-dropdown-badge-inactive"
-                            } menu-dropdown-badge`}
+                              } menu-dropdown-badge`}
                           >
                             new
                           </span>
                         )}
                         {subItem.pro && (
                           <span
-                            className={`ml-auto ${
-                              isActive(subItem.path)
+                            className={`ml-auto ${isActive(subItem.path)
                                 ? "menu-dropdown-badge-active"
                                 : "menu-dropdown-badge-inactive"
-                            } menu-dropdown-badge`}
+                              } menu-dropdown-badge`}
                           >
                             pro
                           </span>
@@ -297,10 +288,9 @@ const AppSidebar: React.FC = () => {
   return (
     <aside
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
-        ${
-          isExpanded || isMobileOpen
-            ? "w-[290px]"
-            : isHovered
+        ${isExpanded || isMobileOpen
+          ? "w-[290px]"
+          : isHovered
             ? "w-[290px]"
             : "w-[90px]"
         }
@@ -310,9 +300,8 @@ const AppSidebar: React.FC = () => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <div
-        className={`py-8 flex ${
-          !isExpanded && !isHovered ? "lg:justify-center" : "justify-start"
-        }`}
+        className={`py-8 flex ${!isExpanded && !isHovered ? "lg:justify-center" : "justify-start"
+          }`}
       >
         <Link to="/">
           {isExpanded || isHovered || isMobileOpen ? (
@@ -334,20 +323,20 @@ const AppSidebar: React.FC = () => {
             </>
           ) : (
             <>
-            <img
-            className="block dark:hidden"
-              src="/images/logo/ThunderGits_Logos/3.png"
-              alt="Logo"
-              width={32}
-              height={32}
-            />
-            <img
-            className="hidden dark:block"
-              src="/images/logo/ThunderGits_Logos/4.png"
-              alt="Logo"
-              width={32}
-              height={32}
-            />
+              <img
+                className="block dark:hidden"
+                src="/images/logo/ThunderGits_Logos/3.png"
+                alt="Logo"
+                width={32}
+                height={32}
+              />
+              <img
+                className="hidden dark:block"
+                src="/images/logo/ThunderGits_Logos/4.png"
+                alt="Logo"
+                width={32}
+                height={32}
+              />
             </>
           )}
         </Link>
@@ -357,11 +346,10 @@ const AppSidebar: React.FC = () => {
           <div className="flex flex-col gap-4">
             <div>
               <h2
-                className={`mb-4 text-xs uppercase flex leading-[20px] text-gray-400 ${
-                  !isExpanded && !isHovered
+                className={`mb-4 text-xs uppercase flex leading-[20px] text-gray-400 ${!isExpanded && !isHovered
                     ? "lg:justify-center"
                     : "justify-start"
-                }`}
+                  }`}
               >
                 {isExpanded || isHovered || isMobileOpen ? (
                   "Menu"
@@ -373,11 +361,10 @@ const AppSidebar: React.FC = () => {
             </div>
             <div className="">
               <h2
-                className={`mb-4 text-xs uppercase flex leading-[20px] text-gray-400 ${
-                  !isExpanded && !isHovered
+                className={`mb-4 text-xs uppercase flex leading-[20px] text-gray-400 ${!isExpanded && !isHovered
                     ? "lg:justify-center"
                     : "justify-start"
-                }`}
+                  }`}
               >
                 {isExpanded || isHovered || isMobileOpen ? (
                   "Others"
--- src/pages/medicine/CreateMedicineForm.tsx
+++ src/pages/medicine/CreateMedicineForm.tsx
@@ -42,20 +42,20 @@ const CreateMedicineForm = () => {
   const navigate = useNavigate();
   const { recentCreatedMedicineId } = useSelector((state: RootState) => state.medicine);
   const { dropdowns, loading: DDLoading } = useSelector((state: RootState) => state.dropDown);
-  const { error,message,success,loading } = useSelector((state: RootState) => state.medicine);
+  const { error, message, success, loading } = useSelector((state: RootState) => state.medicine);
   const [openConfirmationBox, setOpenConfirmationBox] = useState<boolean>(false);
 
   const handleSubmit = async (
     values: typeof initialValues,
     { resetForm }: FormikHelpers<typeof initialValues>
   ) => {
-    
-    const response=await dispatch(createMedicine(values));
+
+    const response = await dispatch(createMedicine(values));
     console.log(response)
-    if ( response.payload.status === 200) {
+    if (response.payload.status === 200) {
       setOpenConfirmationBox(true);
     }
-    
+
     resetForm();
   };
 
@@ -65,25 +65,23 @@ const CreateMedicineForm = () => {
     form: dropdowns.form.length
       ? faker.helpers.arrayElement(dropdowns.form).value
       : '',
-    strength: dropdowns.strength.length
-      ? faker.helpers.arrayElement(dropdowns.strength).value
-      : '',
+    strength: faker.lorem.word(),
     unit: faker.helpers.arrayElement(UNIT_ENUM),
     prescriptionRequired: faker.datatype.boolean(),
   });
-  
- useEffect(() => {
- 
+
+  useEffect(() => {
+
     if (error) {
       toast.error(message);
       // Optionally, clear the error state here if needed
     }
-  
+
     if (success && message) {
       toast.success(message);
       // Optionally, reset success state here if needed
     }
-    return ()=>{
+    return () => {
       dispatch(clearMedicineMessage())
     }
   }, [error, success, message]);
@@ -97,7 +95,7 @@ const CreateMedicineForm = () => {
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
-          // enableReinitialize
+        // enableReinitialize
         >
           {({ isSubmitting, setValues, values, setFieldValue }) => (
             <Form>
@@ -105,77 +103,78 @@ const CreateMedicineForm = () => {
                 {Object.keys(initialValues).map((key) => (
                   <div key={key} className="mb-4">
                     <Label>
-                      {key.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase())}{key==="name"&&<span className='text-red-400' >*</span>}
+                      {key === "form" ? "Medicine Type" : key.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase())}{key === "name" && <span className='text-red-400' >*</span>}
                     </Label>
 
                     {/* React-Select for Form & Strength */}
-                    {key === 'form' || key === 'strength' ? (
-                      <Select
-                      name={key}
-                      options={dropdowns[key]?.map((item: DropdownOption) => ({
-                     label: item.label,
-                     value: item.value,
-                     inputFieldName: item.inputFieldName || '',
-                     status: item.status || ''
-                   })) || [
-                     {
-                         label: '',
-                         value: '',
-                         inputFieldName: '',
-                         status: ''
-                     }
-                   ]}
-                     isLoading={DDLoading}
-                     value={
-                         dropdowns[key]?.find((item: DropdownOption) => item.value === values[key]) || null
-                     }
-                     onChange={(selectedOption) =>
-                         setFieldValue(key, selectedOption ? selectedOption.value : '')
-                     }
-                     className="react-select-container dark:bg-transparent"
-                     classNamePrefix="react-select"
-                     placeholder={`Select ${key}`}
-                 />
-                    
-                    ) : key === 'unit' ? (
+                    {key === 'form'
+                      // || key === 'strength'
+                      ? (
+                        <Select
+                          name={key}
+                          options={dropdowns[key]?.map((item: DropdownOption) => ({
+                            label: item.label,
+                            value: item.value,
+                            inputFieldName: item.inputFieldName || '',
+                            status: item.status || ''
+                          })) || [
+                              {
+                                label: '',
+                                value: '',
+                                inputFieldName: '',
+                                status: ''
+                              }
+                            ]}
+                          isLoading={DDLoading}
+                          value={
+                            dropdowns[key]?.find((item: DropdownOption) => item.value === values[key]) || null
+                          }
+                          onChange={(selectedOption) =>
+                            setFieldValue(key, selectedOption ? selectedOption.value : '')
+                          }
+                          className="react-select-container dark:bg-transparent"
+                          classNamePrefix="react-select"
+                          placeholder={`Select ${key === "form" ? "Medicine Type" : key}`}
+                        />
 
-                      <Select
-                        options={UNIT_ENUM.map((unit) => ({
-                          label: unit.charAt(0).toUpperCase() + unit.slice(1),
-                          value: unit,
-                        }))}
-                        name={key}
-                        isLoading={DDLoading}
-                        value={
-                          UNIT_ENUM.find((unit) => unit === values[key]) ? {
-                            label: values[key].charAt(0).toUpperCase() + values[key].slice(1),
-                            value: values[key],
-                          } : null
-                        }
-                        onChange={(selectedOption) =>
-                          setFieldValue(key, selectedOption ? selectedOption.value : '')
-                        }
-                        className="react-select-container dark:bg-transparent"
-                        classNamePrefix="react-select"
-                        placeholder={`Select ${key}`}/>
-                      
-                    ) : key === 'prescriptionRequired' ? (
-                      <label className="flex items-center cursor-pointer">
-                        <Field type="checkbox" name={key} className="hidden" />
-                        <span className="w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 transition duration-300">
-                          <span
-                            className={`${
-                              values[key] ? 'translate-x-6' : 'translate-x-0'
-                            } bg-white w-4 h-4 rounded-full shadow-md transform duration-300`}
-                          />
-                        </span>
-                      </label>
-                    ) : (
-                      <Field
-                        name={key}
-                        className="h-9 w-full rounded-md border border-gray-300 bg-white px-4 py-2.5 pr-11 text-sm shadow-theme-xs focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white"
-                      />
-                    )}
+                      ) : key === 'unit' ? (
+
+                        <Select
+                          options={UNIT_ENUM.map((unit) => ({
+                            label: unit.charAt(0).toUpperCase() + unit.slice(1),
+                            value: unit,
+                          }))}
+                          name={key}
+                          isLoading={DDLoading}
+                          value={
+                            UNIT_ENUM.find((unit) => unit === values[key]) ? {
+                              label: values[key].charAt(0).toUpperCase() + values[key].slice(1),
+                              value: values[key],
+                            } : null
+                          }
+                          onChange={(selectedOption) =>
+                            setFieldValue(key, selectedOption ? selectedOption.value : '')
+                          }
+                          className="react-select-container dark:bg-transparent"
+                          classNamePrefix="react-select"
+                          placeholder={`Select ${key}`} />
+
+                      ) : key === 'prescriptionRequired' ? (
+                        <label className="flex items-center cursor-pointer">
+                          <Field type="checkbox" name={key} className="hidden" />
+                          <span className="w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 transition duration-300">
+                            <span
+                              className={`${values[key] ? 'translate-x-6' : 'translate-x-0'
+                                } bg-white w-4 h-4 rounded-full shadow-md transform duration-300`}
+                            />
+                          </span>
+                        </label>
+                      ) : (
+                        <Field
+                          name={key}
+                          className="h-9 w-full rounded-md border border-gray-300 bg-white px-4 py-2.5 pr-11 text-sm shadow-theme-xs focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white"
+                        />
+                      )}
 
                     <ErrorMessage name={key} component="div" className="text-red-500 text-sm" />
                   </div>
